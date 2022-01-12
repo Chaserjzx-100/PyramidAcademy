025704989800e4182345df4234678a63fcae8163
Devan <devan_mcneal@yahoo.com>
2022-01-12 16:35:45 -0500

    Set up Unit tests for 3 methods in the class.

---
+++ GenSpark Projects/project2/src/test/java/Project2Test.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class Project2Test {
+
+    @org.junit.jupiter.api.Test
+    void message() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void name() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void randomNum() {
+    }
+}
