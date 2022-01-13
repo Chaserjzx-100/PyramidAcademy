60f4d77507bfff012c3a96c705558c4b6e89d539
Devan <devan_mcneal@yahoo.com>
2022-01-13 11:22:32 -0500

    Test cases created for the 3 methods in the class.

--- GenSpark Projects/project2/src/test/java/Project2Test.java
+++ GenSpark Projects/project2/src/test/java/Project2Test.java
@@ -1,16 +1,36 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class Project2Test {
 
-    @org.junit.jupiter.api.Test
+    @BeforeEach
+    void setUp(){
+        Project2 object = new Project2();
+    }
+
+    @Test
     void message() {
+        String msg = "Hello! What is your first name?";
+        assertEquals(Project2.message().getClass(), String.class);
+        assertEquals(Project2.message(), msg);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void name() {
+        Project2 guessingGame = new Project2();
+        String player = "Devan";
+        String newplayer = guessingGame.name(player);
+        assertTrue(newplayer.contains(player));
+        assertEquals(Project2.name(player).getClass(), String.class);
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void randomNum() {
+        Project2 game = new Project2();
+        int num = game.randomNum();
+        assertNotEquals(num , 21);
     }
 }
