0e35387df542d0f221467ac0a4fff1b085f81cc3
Devan <devan_mcneal@yahoo.com>
2022-02-13 14:06:08 -0500

    Created JUnit tests for intro(), currentScore(), & buffer() methods.

--- GenSpark Projects/hangman/src/test/java/hangmanTest.java
+++ GenSpark Projects/hangman/src/test/java/hangmanTest.java
@@ -1,5 +1,8 @@
+import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +13,20 @@ class hangmanTest {
     void setUp() {
         game = new hangman();
     }
+
+    @Test
+    void intro(){
+        String first = "John";
+        String last = "Cena";
+        String introduction = "Thank you" + first + " " + last + " let's play hangman!";
+        assertEquals(introduction, hangman.intro(first, last));
+    }
+    @Test
+    void currentScore() throws IOException {
+        assertNotEquals(5, hangman.currHighScore());
+    }
+    @Test
+    void buffer(){}
 
     @AfterEach
     void tearDown() {
